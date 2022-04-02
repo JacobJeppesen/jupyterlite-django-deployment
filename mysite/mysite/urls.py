9da0a18a8a91ba0fe26aba03e6675068ade87b39
Jacob Jeppesen <jhj@atla.ai>
2022-04-02 17:21:50 +0200

    Renamed jupy to jupyterliteapp and jupydist to jupyterlitedist

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('jupy.urls')),
+    path('', include('jupyterliteapp.urls')),
     path('admin/', admin.site.urls),
 ]
